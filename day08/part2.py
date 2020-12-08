ee60f875e6f3ef69963054a01f8467db97765f4f
kubasobon <wtty.fool@gmail.com>
2020-12-08 13:00:18 +0100

    solve part 2

--- day08/part2.py
+++ day08/part2.py
@@ -1,34 +1,59 @@
 import string
+import sys
 
 with open("input.txt") as f:
     bootcode = [l.strip(string.whitespace) for l in f.readlines()]
 
-pc = 0
-acc = 0
-visited_pc = []
 
-while True:
-    if pc in visited_pc:
-        print(f"Visited twice: {bootcode[pc]}")
-        print(f"PC: {pc}")
-        print(f"ACC: {acc}")
-        break
-    visited_pc.append(pc)
+# test_bootcode = [
+#     "nop +0",
+#     "acc +1",
+#     "jmp +4",
+#     "acc +3",
+#     "jmp -3",
+#     "acc -99",
+#     "acc +1",
+#     "jmp -4",
+#     "acc +6",
+# ]
+# bootcode = test_bootcode
 
-    if pc >= len(bootcode):
-        print(f"PC exceeded bootcode length: {pc} >= {len(bootcode)}")
-        break
 
-    opcode, arg = bootcode[pc].split()
-    arg = int(arg)
-
-    if opcode == "acc":
-        acc += arg
-        pc += 1
+for change_pc in range(len(bootcode) - 1, -1, -1):
+    if bootcode[change_pc].startswith("acc"):
         continue
-    if opcode == "jmp":
-        pc += arg
-        continue
-    if opcode == "nop":
-        pc += 1
-        continue
+
+    pc = 0
+    acc = 0
+    visited_pc = []
+    while True:
+        if pc in visited_pc:
+            break  # infinite loop
+        visited_pc.append(pc)
+
+        if pc == len(bootcode):
+            print(f"Bootcode terminated correctly")
+            print(f"Changed opcode at: {change_pc}")
+            print(f"PC: {pc}")
+            print(f"ACC: {acc}")
+            sys.exit()
+
+        opcode, arg = bootcode[pc].split()
+        arg = int(arg)
+
+        if pc == change_pc:
+            if opcode == "jmp":
+                opcode = "nop"
+            elif opcode == "nop":
+                opcode = "jmp"
+
+        if opcode == "acc":
+            acc += arg
+            pc += 1
+            continue
+        if opcode == "jmp":
+            pc += arg
+            continue
+        if opcode == "nop":
+            pc += 1
+            continue

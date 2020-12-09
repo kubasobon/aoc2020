605173888893c09cf07fb61c38f3f9763f6829d7
kubasobon <wtty.fool@gmail.com>
2020-12-09 11:56:54 +0100

    part 1

---
+++ day09/part1.py
@@ -0,0 +1,24 @@
+with open("input.txt") as f:
+    data = [int(i) for i in f.readlines()]
+
+
+preamble_len = 25
+pos = preamble_len
+results = {}
+
+while pos < len(data):
+    numbers = {}.fromkeys(data[pos-preamble_len:pos])
+    key = data[pos]
+
+    match = False
+    for n in numbers:
+        n2 = key - n
+        if n2 == n:
+            continue
+        if n2 in numbers:
+            break
+    else:
+        print(f"Could not factor {key}")
+        break
+
+    pos += 1

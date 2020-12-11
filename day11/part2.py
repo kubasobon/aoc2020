2d4cc6b8090d364fe83465728e710ceb7a4a8357
kubasobon <wtty.fool@gmail.com>
2020-12-11 16:32:23 +0100

    part 2

--- day11/part2.py
+++ day11/part2.py
@@ -16,10 +16,12 @@ def trace(state, x, y, dx, dy, max_x, max_y):
         y += dy
         if x >= max_x or y >= max_y:
             return False
-        if x <= 0 or y <= 0:
+        if x < 0 or y < 0:
             return False
         if state[y][x] == "#":
             return True
+        if state[y][x] == "L":
+            return False
 
 
 def next_state(state):
@@ -28,42 +30,60 @@ def next_state(state):
     future = []
 
     for y, row in enumerate(state):
+        frow = []
         for x, ch in enumerate(row):
-            row = []
             if ch == ".":
-                row.append(ch)
+                frow.append(ch)
                 continue
+
             taken = 0
+
             for d in directions:
                 dx, dy = d
                 if trace(state, x, y, dx, dy, cols, rows):
                     taken += 1
 
             if ch == "#" and taken >= 5:
-                row.append("L")
+                frow.append("L")
             elif ch == "L" and taken == 0:
-                row.append("#")
+                frow.append("#")
             else:
-                row.append(ch)
-        future.append(row)
+                frow.append(ch)
+
+        future.append("".join(frow))
     return future
 
 def flattened(state):
-    return "".join("".join(l) for l in state)
+    return "".join(state)
 
 with open("input.txt") as f:
     state = [l.strip(string.whitespace) for l in f.readlines()]
 
+
+state = [
+    "L.LL.LL.LL",
+    "LLLLLLL.LL",
+    "L.L.L..L..",
+    "LLLL.LL.LL",
+    "L.LL.LL.LL",
+    "L.LLLLL.LL",
+    "..L.L.....",
+    "LLLLLLLLLL",
+    "L.LLLLLL.L",
+    "L.LLLLL.LL",
+]
+
+
 prev_state = []
 iterations = 0
 
-while flattened(prev_state) != flattened(state):
-    prev_state = state
-    state = future_state(state)
+while prev_state != flattened(state):
+    prev_state = flattened(state)
+    state = next_state(state)
     iterations += 1
 
 print(f"Stable after {iterations} iterations")
-seats_occupied = state.count("#")
+seats_occupied = sum(l.count("#") for l in state)
 print(f"Occupied seats: {seats_occupied}")
 
 

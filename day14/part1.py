e2b050c41e62d7eac17542d1c8f9ccd54e1c4f51
kubasobon <wtty.fool@gmail.com>
2020-12-15 17:40:25 +0100

    add part 1 solution

---
+++ day14/part1.py
@@ -0,0 +1,35 @@
+import string
+
+def apply_mask(mask, dec):
+    b = bin(dec)[2:].zfill(36)
+    new_b = []
+    for i, v in enumerate(mask):
+        if v == "X":
+            new_b.append(b[i])
+            continue
+        new_b.append(v)
+    return int(b, 2)
+
+
+with open("input.txt") as f:
+    data = [l.strip(string.whitespace) for l in f.readlines()]
+
+mask = ""
+mem = {}
+for l in data:
+    if l.startswith("mask"):
+        mask = l.split(" = ")[1]
+    else:
+        mem_prefix, v = l.split(" = ")
+        v = apply_mask(mask, int(v))
+        address = int(mem_prefix.strip("me[]"))
+        mem[address] = v
+
+
+print("--- memory ---")
+for k, v in mem.items():
+    print(f"{k}:\t{v}")
+print("--------------")
+
+result = sum(mem.values())
+print(f"Sum of memory values: {result}")

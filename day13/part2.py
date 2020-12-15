3e2b7bcaff26697d43038987a6af77b1d97ef732
kubasobon <wtty.fool@gmail.com>
2020-12-15 17:22:53 +0100

    find common multipliers

--- day13/part2.py
+++ day13/part2.py
@@ -2,26 +2,15 @@ with open("input.txt") as f:
     f.readline()
     raw_conditions = enumerate(f.readline().split(","))
 
-conditions = {
-    int(c): int(c) - (i % int(c)) if i > 0 else 0 for i, c in raw_conditions if c != "x"
-}
-
-# conditions = {67: 0, 7: 6, 59: 57, 61: 58}
-# conditions = {1789: 0, 37: 37 - 1, 47: 47 - 2, 1889: 1889 - 3}
-print(f"Conditions: {conditions}")
-
-t = max(conditions) + 1
-jmp = 0
-for k, v in conditions.items():
-    if k < t and v == 0:
-        t = k
-        jmp = k
-
-
-print(f"t: {t}, jmp: {jmp}")
-while True:
-    if not all(t % k == v for k, v in conditions.items()):
-        t += jmp
+t = 0
+jmp = 1
+for i, b in raw_conditions:
+    if b == "x":
         continue
-    print(f"Matched positions at {t}")
-    break
+
+    while (t + i) % int(b) != 0:
+        t += jmp
+
+    jmp *= int(b)
+
+print(f"Result: {t}")

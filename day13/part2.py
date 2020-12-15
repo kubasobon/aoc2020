d288e54899b181eb3e281b6d3d1934d982259e60
kubasobon <wtty.fool@gmail.com>
2020-12-15 16:29:59 +0100

    solve part 2

--- day13/part2.py
+++ day13/part2.py
@@ -2,11 +2,16 @@ with open("input.txt") as f:
     f.readline()
     conditions = {i: int(t) for i, t in enumerate(f.readline().split(",")) if t != "x"}
 
+conditions = {0: 17, 2: 13, 3: 19}
+conditions_stop = max(conditions) + 1
+
 t = 0
 while True:
     match = True
-    for i, key in enumerate(conditions):
-        if (t + i) % conditions[key] > 0:
+    for i in range(conditions_stop):
+        if i not in conditions:
+            continue
+        if (t + i) % conditions[i] > 0:
             match = False
             break
     if match:
